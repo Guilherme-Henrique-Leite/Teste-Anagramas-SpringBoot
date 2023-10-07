6709c7655859126fe860eed693358619e49f0f4f
Guilherme Henrique Leite de Albuquerque <guilherme.albuquerque@mv.com.br>
2023-10-06 21:46:15 -0300

    Teste Concluído!

---
+++ Chamada-teste/src/test/java/com/projeto/Chamada/teste/ChamadaTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.Chamada.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadaTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ chamada-teste-java/src/main/java/br/com/springboot/chamada_teste_java/Application.java
@@ -0,0 +1,15 @@
+package br.com.springboot.chamada_teste_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);/*Rodar projeto */
+    }
+}
